39c9690b861ad8a38b18b364f12e012cc8930d15
Rodoudou <amrani.redouane@gmail.com>
2021-09-19 02:51:27 +0200

    last commit

--- script.js
+++ script.js
@@ -1,13 +1,26 @@
-const slide = new Array("./assets/img/capture.png", "./assets/img/redouuu.jpeg", "./assets/img/redcolor.jpg");
-let numero = 0;
+const ArrayImages = [
+  "./assets/img/capture.png",
+  './assets/img/lol.png',
+  "./assets/img/redcolor.jpg",
+  "./assets/img/redouuu.jpeg",
+];
 
+let position = 0;
 
-const ChangeSlide = (sens)=>{
-    numero = numero + sens;
-    (numero <0)?(numero = slide.length - 1): null;
-    (numero > slide.length - 1)? (numero = 0): null;
+const image = document.getElementById("image");
 
-    document.getElementById("slide").src = slide[numero];
+const suivant = document.getElementById("suivant");
+const precedent = document.getElementById("precedent");
+
+const slideChange=(sens)=>{
+  position = position + sens;
+  // fleche de  gauche : si ma position est inferieur à 0 alors je me positionne sur l'avant derniere img
+  (position<0)?(position = ArrayImages.length -1): null;
+  // fleche de  droite : si ma position est superieur à 0 alors j'avance
+  (position>ArrayImages.length-1)?(position=0): null;
+  // attribuer une image à la balise img
+  image.src = ArrayImages[position];
+  console.log("ArrayImages.length -1",ArrayImages.length -1);
+  console.log("ArrayImages.length -1",ArrayImages[position] );
 }
 
-// setInterval("ChangeSlide(1)", 4000);
